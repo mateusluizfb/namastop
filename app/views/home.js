3a2692de9c4e1edfb7a2498fcbbb84852f209aab
Mateus Luiz <mateuslfreitasb@gmail.com>
2019-01-03 12:30:14 -0200

    Styling the cards

--- app/views/home.js
+++ app/views/home.js
@@ -3,6 +3,25 @@ const Imports = require('./imports');
 const CardsTable = require('./cards_table');
 
 class Home extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { activePage: 1 };
+
+    this.arrowRightStyle = {
+      'position': 'absolute',
+      'top': '50%',
+      'right': '10%'
+    }
+
+    this.arrowLeftStyle = {
+      'position': 'absolute',
+      'top': '50%',
+      'left': '10%',
+      'color': '#d0d3d8'
+    }
+  }
+
   render() {
     return (
       <div>
@@ -13,11 +32,20 @@ class Home extends React.Component {
           </div>
         </nav>
 
-        <div className="container" style={{'marginTop': '1.0em'}}>
-          <div className="row">
-            <CardsTable gratitudeMessages={this.props.gratitudeMessages} />
+        <div className="row">
+          <div className="col m1">
+            <i style={this.arrowLeftStyle} class="fas fa-arrow-left fa-5x"/>
+          </div>
+          <div className="col m10">
+            <div className="container" style={{'marginTop': '1.0em'}}>
+              <CardsTable gratitudeMessages={this.props.gratitudeMessages} />
+            </div>
+          </div>
+          <div className="col m1">
+            <i style={this.arrowRightStyle} class="fas fa-arrow-right fa-5x"/>
           </div>
         </div>
+
       </div>
     );
   }
